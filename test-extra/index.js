2d256db313d3eb8ad54c822f2d992f3d616b6159
sean billig <sean.billig@gmail.com>
2020-12-20 13:35:54 -0800

    add failing test for loadOrCreate difference from ssb-keys

--- test-extra/index.js
+++ test-extra/index.js
@@ -1,9 +1,23 @@
-var tape = require('tape');
+let tape = require('tape');
+let ssbKeys = require('../');
+let path = require('path');
+let os = require('os');
+let fs = require('fs');
 
 // This file was used when ssb-keys was lacking tests for some public APIs.
 // Nowadays we update ssb-keys tests to cover everything.
 // You can still use this file to add ad-hoc tests if you need
 
-tape('no extra tests', function (t) {
-  t.end();
+const keyPath = path.join(os.tmpdir(), `ssb-keys-${Date.now()}`);
+
+tape("don't create dir for fully-specified path", function (t) {
+  ssbKeys.loadOrCreate(keyPath, (err, keys) => {
+    t.error(err);
+    t.true(fs.lstatSync(keyPath).isFile());
+
+    ssbKeys.loadOrCreate(keyPath, (err, keys) => {
+      t.equal(keys.public.length, 52);
+      t.end();
+    });
+  });
 });
